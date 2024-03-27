e41a63e321aa0b9107a4d956ad9e44c22a83a4ea
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-03-27 01:35:54 -0700

    Modified Expansion to use Automatic workflow

--- src/AnimationMenus/expansionContraction.lua
+++ src/AnimationMenus/expansionContraction.lua
@@ -28,17 +28,8 @@ function ExpansionContractionMenu()
 
             local distance = mapProgress(settings.distance[1], progress, settings.distance[2])
 
-            local msx = settings.msxBounds[1]
-            local iterations = 0
-
-            local msxTable = {}
-
-            while (msx <= settings.msxBounds[2]) do
-                table.insert(msxTable, msx)
-                msx = msx + distance
-            end
-
-            local tbl = returnFixedLines(msxTable, currentTime, 0, settings.spacing)
+            local tbl = placeAutomaticFrame(currentTime, settings.msxBounds[1], settings.msxBounds[2], settings.spacing,
+                { distance, distance })
 
             if (tbl.time > offsets.endOffset) then break end
 
@@ -53,6 +44,7 @@ function ExpansionContractionMenu()
 
             currentTime = currentTime + 2
         end
+
         svs = cleanSVs(svs, offsets.startOffset, offsets.endOffset)
 
         settings.debug = #lines .. ' // ' .. #svs
