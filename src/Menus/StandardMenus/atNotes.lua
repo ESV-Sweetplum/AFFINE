0f0deaf30902ea9ab5dbafa9b25e69322f633978
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-03-30 17:12:05 -0700

    Removed debugs from atNotes

--- src/Menus/StandardMenus/atNotes.lua
+++ src/Menus/StandardMenus/atNotes.lua
@@ -1,24 +1,17 @@
 function StandardAtNotesMenu()
     local offsets = getSelectedOffsets()
 
-    local color = state.GetValue("color") or 69
-
-    imgui.Text(color)
-
     if NoteActivated(offsets) then
         local lines = {}
 
         if (type(offsets) == "integer") then return end
 
         for _, offset in pairs(offsets) do
-            color = colorFromTime(offset)
-            lines = concatTables(lines, applyColorToTime(color, offset))
+            lines = concatTables(lines, keepColorLine(offset))
         end
 
         lines = cleanLines(lines, offsets[1], offsets[#offsets] + 10)
 
         actions.PlaceTimingPointBatch(lines)
     end
-
-    state.SetValue("color", color)
 end
