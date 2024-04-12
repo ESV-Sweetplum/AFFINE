848636bf91cb2d5c051b4f8b55dd0c155c759662
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-04-12 10:17:09 -0700

    Include temporal bm condition

--- src/lib/state/getMapState.lua
+++ src/lib/state/getMapState.lua
@@ -1,7 +1,7 @@
 function getMapState(default)
     default = default or {}
     if (not map.Bookmarks[1]) then return default end
-    if (not string.find(map.Bookmarks[1].note, "DATA: ")) then return default end
+    if (not string.find(map.Bookmarks[1].note, "DATA: ") or not map.Bookmarks[1].StartTime == -69420) then return default end
 
     local str = map.Bookmarks[1].note:sub(7, map.Bookmarks[1].note:len())
 
