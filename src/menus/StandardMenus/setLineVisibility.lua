ad79f32f80ab8030bf0f12ddac1d19ea119d8516
ESV Sweetplum <sweetplumquaver@gmail.com>
2024-04-17 23:35:42 -0700

    New set line visibility menu

---
+++ src/menus/StandardMenus/setLineVisibility.lua
@@ -0,0 +1,32 @@
+function SetVisibilityMenu()
+    local parameterTable = constructParameters(
+        {
+            inputType = "RadioBoolean",
+            key = "enable",
+            label = { "Turn Lines Invisible", "Turn Lines Visible" },
+            value = false
+        })
+
+    retrieveParameters("set_visibility", parameterTable)
+
+    parameterInputs(parameterTable)
+    local settings = parametersToSettings(parameterTable)
+    local offsets = getStartAndEndNoteOffsets()
+
+    if RangeActivated(offsets) then
+        local linesToRemove = getLinesInRange(offsets.startOffset, offsets.endOffset)
+
+        local linesToAdd = {}
+
+        for _, currentLine in pairs(linesToRemove) do
+            table.insert(linesToAdd, line(currentLine.StartTime, currentLine.Bpm, not settings.enable))
+        end
+
+        actions.PerformBatch({
+            utils.CreateEditorAction(action_type.RemoveTimingPointBatch, linesToRemove),
+            utils.CreateEditorAction(action_type.AddTimingPointBatch, linesToAdd)
+        })
+    end
+
+    saveParameters("set_visibility", parameterTable)
+end
