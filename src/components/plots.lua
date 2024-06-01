b66406faf78e922b00f6435b7f703f8a150b196d
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2024-06-01 23:39:08 +0800

    Add a sinusoidal plot option

--- src/components/plots.lua
+++ src/components/plots.lua
@@ -30,6 +30,30 @@ function PolynomialPlot(polynomialCoefficients, progressionExponent, title)
     imgui.End()
 end
 
+---Plots a sinusoidal graph given cycle counts and phase shift.
+---@param nx [number, number] # Start and End cycle counts. The sine function will have `nx[2]` cycles in total.
+---@param phi number # Phase shift represented as a number in [0,1).
+---@param title string # Title of the plot window.
+function SinusoidalPlot(nx, phi, title)
+    imgui.Begin(title or "Boundary Height vs. Time", imgui_window_flags.AlwaysAutoResize)
+
+    local tbl = {}
+
+    for i = 0, RESOLUTION do
+        local x = i / RESOLUTION
+        local fn = function (v) return nx[1] + (nx[2] - nx[1]) * v end
+
+        table.insert(tbl, math.sin(2 * math.pi * (x * fn(x) + phi)))
+    end
+
+    imgui.PlotLines("Sinusoidal Plot", tbl, #tbl, 0,
+        "",
+        -1, 1,
+        { 250, 150 })
+
+    imgui.End()
+end
+
 function Plot(fn, label, windowName)
     imgui.Begin(windowName or "Boundary Height vs. Time", imgui_window_flags.AlwaysAutoResize)
 
